5ace698812e1bb189764b0cf8c0df3bd260bc1a5
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2023-03-26 17:04:51 +0530

    updating the index.html style.css and script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-alert('Hell World!')
+function getProductDetails() {
+  fetch(`https://dummyjson.com/products`)
+    .then((res) => res.json())
+    .then((data) => {
+      for (var i = 0; i < data.length; i++) {
+        alert(data[i].thumbnail)
+        card +=
+          '<div class="type-sitdown card col-md-5">' +
+          '<img src="' +
+          data[i].thumbnail +
+          '" class="card-image card-img-top" alt="restaurant-image">' +
+          '<div class="card-body" id="card-details">' +
+          '<h5 class="card-title">' +
+          data[i].category +
+          "</h5>";
+      }
+    });
+
+    document.getElementById("card-details").innerHTML = card;
+}
