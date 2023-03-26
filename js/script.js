375271a2b5e5631c0c160d2b5baf72369ceccbbb
Sanpreet Singh <sanpreet.india@gmail.com>
2023-03-26 18:50:11 +0530

    commiting sidebar

--- js/script.js
+++ js/script.js
@@ -18,3 +18,31 @@ function getProductDetails() {
 
     document.getElementById("card-details").innerHTML = card;
 }
+
+function getSmartphoneCategory(){
+  fetch(`https://dummyjson.com/products/category/smartphones`)
+    .then((res) => res.json())
+    .then((data) => {
+      // for (var i = 0; i < data.length; i++) {
+      //   alert(data[i].thumbnail)
+      //   card +=
+      //     '<div class="type-sitdown card col-md-5">' +
+      //     '<img src="' +
+      //     data[i].thumbnail +
+      //     '" class="card-image card-img-top" alt="restaurant-image">' +
+      //     '<div class="card-body" id="card-details">' +
+      //     '<h5 class="card-title">' +
+      //     data[i].category +
+      //     "</h5>";
+      // }
+
+      // console.log(data);
+
+
+      for(var i = 0; i < data.length; i++){
+        console.log(data[i].products.title);
+      }
+    });
+}
+getSmartphoneCategory();
+
