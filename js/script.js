5acd0f9a0d26c938e01f447487da5f11fd191481
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2023-03-27 08:58:10 +0530

    added card structure for the landing page (Home)

--- js/script.js
+++ js/script.js
@@ -1,48 +1,53 @@
 function getProductDetails() {
+  var card="";
   fetch(`https://dummyjson.com/products`)
     .then((res) => res.json())
     .then((data) => {
-      for (var i = 0; i < data.length; i++) {
-        alert(data[i].thumbnail)
-        card +=
-          '<div class="type-sitdown card col-md-5">' +
+      for (var i = 0; i < data.products.length; i++) {
+        card += 
+          '<div class="card col-md-5">' +
           '<img src="' +
-          data[i].thumbnail +
+          data.products[i].thumbnail +
           '" class="card-image card-img-top" alt="restaurant-image">' +
           '<div class="card-body" id="card-details">' +
           '<h5 class="card-title">' +
-          data[i].category +
-          "</h5>";
+          data.products[i].category +
+          "</h5></div></div>";
+          
       }
+      document.getElementById("card-details").innerHTML = card;
+      
     });
-
-    document.getElementById("card-details").innerHTML = card;
+    // console.log(card);
+    // 
 }
 
-function getSmartphoneCategory(){
-  fetch(`https://dummyjson.com/products/category/smartphones`)
-    .then((res) => res.json())
-    .then((data) => {
-      // for (var i = 0; i < data.length; i++) {
-      //   alert(data[i].thumbnail)
-      //   card +=
-      //     '<div class="type-sitdown card col-md-5">' +
-      //     '<img src="' +
-      //     data[i].thumbnail +
-      //     '" class="card-image card-img-top" alt="restaurant-image">' +
-      //     '<div class="card-body" id="card-details">' +
-      //     '<h5 class="card-title">' +
-      //     data[i].category +
-      //     "</h5>";
-      // }
+getProductDetails();
 
-      // console.log(data);
+// function getSmartphoneCategory(){
+//   fetch(`https://dummyjson.com/products/category/smartphones`)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       // for (var i = 0; i < data.length; i++) {
+//       //   alert(data[i].thumbnail)
+//       //   card +=
+//       //     '<div class="type-sitdown card col-md-5">' +
+//       //     '<img src="' +
+//       //     data[i].thumbnail +
+//       //     '" class="card-image card-img-top" alt="restaurant-image">' +
+//       //     '<div class="card-body" id="card-details">' +
+//       //     '<h5 class="card-title">' +
+//       //     data[i].category +
+//       //     "</h5>";
+//       // }
+
+//       console.log(data.products.length);
 
 
-      for(var i = 0; i < data.length; i++){
-        console.log(data[i].products.title);
-      }
-    });
-}
-getSmartphoneCategory();
+//       for(var i = 0; i < data.products.length; i++){
+//         console.log(data.products[i].title);
+//       }
+//     });
+// }
+// getSmartphoneCategory();
 
